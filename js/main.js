acf8b11ee76dfe4fdc606fc06439121398bd6a0e
Jasper Boyd <jasperboyd@lavabit.com>
2013-04-18 23:39:27 -0400

    setup .js , more styles and content

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-
+$(document).ready(function(){
+	//top nav
+	$home = $("#home");
+	$about = $("#about");
+	$reviews = $("#reviews");
+	$submit = $("#submit");
+	$contact = $("#contact");
+	
+	$upper_nav = $("nav a");
+	
+	//welcome page buttons
+	$join = $("#join");
+	$read = $("#read"); 
+	$listen = $("#listen");
+	$submit_button = $("#submit_button");
+	
+	//content
+	$socialmedia = $("#socialmedia"); 
+	
+	$join.click(function(){
+		$socialmedia.fadeToggle();
+		$join.toggleClass("button_sel");
+	});
+	
+	selector = -1; 
+	
+	$upper_nav.click(function(){
+		
+	});
+	
+	
+});
